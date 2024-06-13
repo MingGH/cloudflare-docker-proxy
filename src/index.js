83649fcfe46f7d138364a4e17d3d366c39bafe57
MingGH <1558772318@qq.com>
2024-06-13 20:15:06 +0800

    update domain

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.996.ninja": "https://registry-1.docker.io",
+  "quay.996.ninja": "https://quay.io",
+  "gcr.996.ninja": "https://gcr.io",
+  "k8s-gcr.996.ninja": "https://k8s.gcr.io",
+  "k8s.996.ninja": "https://registry.k8s.io",
+  "ghcr.996.ninja": "https://ghcr.io",
+  "cloudsmith.996.ninja": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
